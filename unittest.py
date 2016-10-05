d0ae3c555d8859fbe17ee9aa89a0c70ce4f5eed6
Sruthi Gottumukkala <shruti.gottumukkala@gmail.com>
2016-10-05 10:53:56 -0700

    added new

---
+++ unittest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import unittest
+from simpleapp import hello_world
+
+class HelloWorldTestCase(unittest.TestCase):
+    """Tests for `simpleapp.py`."""
+
+    def test_is_output_hw(self):
+        """Is the output of your Python Application what you expect?"""
+        self.assertTrue(hello_world() == "Hello World from Distelli!")
+
+if __name__ == '__main__':
+    unittest.main()
